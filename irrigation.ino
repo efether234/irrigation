afeea6f10c760576dff243bfdc11fb0e930acdf0
Evan Fetherolf <efether234@gmail.com>
2022-04-28 13:54:12 -0700

    Add LED to emulate valve function.

--- irrigation.ino
+++ irrigation.ino
@@ -1,14 +1,31 @@
 const int READPIN = 0;
+const int LEDPIN = 13;
 int sensorVal;
 int moistPer;
+int lowPer = 70;
+int highPer = 80;
 
 void setup() {
+    pinMode(LEDPIN, OUTPUT);
     Serial.begin(9600);
+    delay(1000);
 }
 
 void loop() {
-    sensorVal = analogRead(READPIN;
-    moistPer = map(sensorVal, 570, 295, 0, 100);
+    sensorVal = analogRead(READPIN);
+    moistPer = map(sensorVal, 650, 295, 0, 100);
+    Serial.print(sensorVal);
+    Serial.print(" | ");
     Serial.print(moistPer);
     Serial.println("%");
+
+    if (moistPer < lowPer) {
+        digitalWrite(LEDPIN, 1);
+    }
+
+    if (moistPer > highPer) {
+        digitalWrite(LEDPIN, 0);
+    }
+
+    delay(10);
 }
